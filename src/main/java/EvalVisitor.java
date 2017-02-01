6d3a40a04b41989d2e948670c38d9fb3c4ce2ca6
zhewang711 <zhw176@ucsd.edu>
2017-01-31 16:53:27 -0800

    add loading

--- src/main/java/EvalVisitor.java
+++ src/main/java/EvalVisitor.java
@@ -1,7 +1,58 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileInputStream;
 import java.util.ArrayList;
+
 
 /**
  * Created by zhewang711 on 1/31/17.
  */
+
 public class EvalVisitor extends XPathBaseVisitor<ArrayList<Object>> {
+
+    private Node n = null; // parameter node
+
+    /* Java XML Reference: http://tutorials.jenkov.com/java-xml/dom.html */
+    // shift tab -- adjust space
+    // control alt o -- optimizing import
+    // control + delete -- delete a line
+    private Node load_doc(String fileName) throws Exception {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        Document document;
+
+        builder = builderFactory.newDocumentBuilder();
+        document = builder.parse(new FileInputStream(fileName));
+        return document.getDocumentElement();
+    }
+
+    @Override
+    public ArrayList<Object> visitAbs_slash(XPathParser.Abs_slashContext ctx) {
+        ArrayList<Object> res = null;
+        try{
+            n = load_doc(ctx.FILENAME().getText());
+            res = visit(ctx.re_path());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        n = null;
+        return res;
+    }
+
+    @Override
+    public ArrayList<Object> visitText(XPathParser.TextContext ctx){
+        System.out.println("text visited!");
+        return null;
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/XPathProcessor.java
+++ src/main/java/XPathProcessor.java
@@ -1,6 +1,10 @@
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.gui.TreeViewer;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 /**
  * Created by zhewang711 on 1/31/17.
@@ -11,7 +15,7 @@ public class XPathProcessor {
     public static void main(String[] args) {
 
         try {
-            ANTLRInputStream input = new ANTLRInputStream("doc(\"x.xml\")/a/text()");
+            ANTLRInputStream input = new ANTLRInputStream("doc(\"j_caesar.xml\")/text()");
             XPathLexer lexer = new XPathLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
 
@@ -19,6 +23,22 @@ public class XPathProcessor {
             parser.removeErrorListeners();
             ParseTree tree = parser.abs_path();
             EvalVisitor evalVisitor = new EvalVisitor();
+
+            System.out.println(tree.toStringTree(parser));
+
+//            JFrame frame = new JFrame("Antlr AST");
+//            JPanel panel = new JPanel();
+//            TreeViewer viewr = new TreeViewer(Arrays.asList(
+//                    parser.getRuleNames()),tree);
+//            viewr.setScale(1.5);//scale a little
+//            panel.add(viewr);
+//            frame.add(panel);
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            frame.setSize(200,200);
+//            frame.setVisible(true);
+
+
+
             evalVisitor.visit(tree);
 
         } catch (Exception e) {
